c44de4ef80e5098218214bf6ede0b17ee37de713
JosephJasin <Josef20606@gmail.com>
2020-09-16 08:18:57 +0300

    initalize firebase

--- mobile_app/lib/pages/pages.dart
+++ mobile_app/lib/pages/pages.dart
@@ -7,6 +7,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 import '../themes.dart';
 import '../widgets.dart';
